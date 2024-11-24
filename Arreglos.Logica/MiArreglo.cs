9e1bf5e7e5e68052e2f3211c8300c42a37cb3bb7
SebastianRSS04 <22030619@itesa.edu.mx>
2024-11-23 23:48:15 -0600

    Version 03: Se agregaron los metodos al menu, se realizaron correccines y se implemento complementos

--- Arreglos.Logica/MiArreglo.cs
+++ Arreglos.Logica/MiArreglo.cs
@@ -2,11 +2,11 @@
 {
     public class MiArreglo
     {
-        //Campos o stributo
+        // Campos o atributo
         private int _tope;
         private int[] _arreglo;
 
-        //Constructor
+        // Constructor
         public MiArreglo(int n)
         {
             N = n;
@@ -14,12 +14,30 @@
             _tope = 0;
         }
 
-        //Propiedades 
+        // Propiedades
         public int N { get; }
         public bool EstaLLeno => _tope == N;
         public bool EstaVacio => _tope == 0;
+        public bool EstaOrdenado { get; private set; } = false;
 
+        // Métodos de llenado y manipulación del arreglo
+        public void Llenar()
+        {
+            Llenar(1, 100); // Por defecto llena con números entre 1 y 100
+        }
 
+        public void Llenar(int minimo, int maximo)
+        {
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                _arreglo[i] = random.Next(minimo, maximo);
+            }
+
+            _tope = N;
+        }
+
+        // Métodos de obtención solicitados
         public MiArreglo ObtenerPares()
         {
             int contadorPares = 0;
@@ -28,10 +46,8 @@
             {
                 if (_arreglo[i] % 2 == 0)
                 {
-
                     contadorPares++;
                 }
-
             }
 
             var arregloPares = new MiArreglo(contadorPares);
@@ -40,12 +56,10 @@
             {
                 if (_arreglo[i] % 2 == 0)
                 {
-
                     arregloPares.Agregar(_arreglo[i]);
                 }
             }
             return arregloPares;
-
         }
 
         public MiArreglo ObtenerPrimos()
@@ -57,7 +71,6 @@
                 {
                     contadorPrimos++;
                 }
-
             }
             var arregloPrimos = new MiArreglo(contadorPrimos);
             for (int i = 0; i < _tope; i++)
@@ -77,48 +90,78 @@
                 if (numero % i == 0)
                 {
                     return false;
-
                 }
             }
             return true;
-
         }
 
-        public void Llenar()
+        public MiArreglo ObtenerUnicos()
         {
-            Llenar(1, 100);
-        }
+            var unicos = new MiArreglo(_tope);
+            var contador = new Dictionary<int, int>();
 
-        public void Llenar(int minimo, int maximo)
-        {
-            Random random = new Random();
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < _tope; i++)
             {
-                _arreglo[i] = random.Next(minimo, maximo);
+                if (contador.ContainsKey(_arreglo[i]))
+                    contador[_arreglo[i]]++;
+                else
+                    contador[_arreglo[i]] = 1;
             }
 
-            _tope = N;
+            foreach (var item in contador)
+            {
+                if (item.Value == 1)
+                    unicos.Agregar(item.Key);
+            }
+
+            return unicos;
         }
 
-        public void Ordenar() //metodo burbuja
+        public MiArreglo ObtenerMasFrecuentes()
         {
+            var frecuentes = new MiArreglo(_tope);
+            var contador = new Dictionary<int, int>();
 
-            Ordenar(true);
+            for (int i = 0; i < _tope; i++)
+            {
+                if (contador.ContainsKey(_arreglo[i]))
+                    contador[_arreglo[i]]++;
+                else
+                    contador[_arreglo[i]] = 1;
+            }
+
+            int maxFrecuencia = contador.Values.Max();
+
+            foreach (var item in contador)
+            {
+                if (item.Value == maxFrecuencia)
+                    frecuentes.Agregar(item.Key);
+            }
+
+            return frecuentes;
         }
 
-        public void Ordenar(bool asendente)
+        //----------------------------------------------------
+        // Métodos de ordenamiento solicitados en la asignación
+        //---------------------------------------------------
+
+        public void Ordenar()
+        {
+            Ordenar(true); // Método burbuja por defecto
+        }
+
+        public void Ordenar(bool ascendente)
         {
             for (int i = 0; i < _tope - 1; i++)
             {
                 for (int j = i + 1; j < _tope; j++)
                 {
-                    if (asendente)
+                    if (ascendente)
                     {
                         if (_arreglo[i] < _arreglo[j])
                         {
                             Cambiar(ref _arreglo[i], ref _arreglo[j]);
                         }
-
                     }
                     else
                     {
@@ -128,9 +171,145 @@
                         }
                     }
                 }
-
             }
+            EstaOrdenado = true;
         }
+
+        // Ordenación por Inserción.
+        public void OrdenarInsercion()
+        {
+            for (int i = 1; i < _tope; i++)
+            {
+                int aux = _arreglo[i];
+                int j = i - 1;
+
+                while (j >= 0 && _arreglo[j] > aux)
+                {
+                    _arreglo[j + 1] = _arreglo[j];
+                    j--;
+                }
+
+                _arreglo[j + 1] = aux;
+            }
+            EstaOrdenado = true;
+        }
+
+        // Ordenación por Selección.
+        public void OrdenarSeleccion()
+        {
+            for (int i = 0; i < _tope - 1; i++)
+            {
+                int indiceMenor = i;
+                for (int j = i + 1; j < _tope; j++)
+                {
+                    if (_arreglo[j] < _arreglo[indiceMenor])
+                    {
+                        indiceMenor = j;
+                    }
+                }
+                if (i != indiceMenor)
+                {
+                    Cambiar(ref _arreglo[i], ref _arreglo[indiceMenor]);
+                }
+            }
+            EstaOrdenado = true;
+        }
+
+        // Ordenación por Shell.
+        public void OrdenarShell()
+        {
+            int intervalo = _tope / 2;
+            while (intervalo > 0)
+            {
+                for (int i = intervalo; i < _tope; i++)
+                {
+                    int temp = _arreglo[i];
+                    int j = i;
+
+                    while (j >= intervalo && _arreglo[j - intervalo] > temp)
+                    {
+                        _arreglo[j] = _arreglo[j - intervalo];
+                        j -= intervalo;
+                    }
+
+                    _arreglo[j] = temp;
+                }
+
+                intervalo /= 2;
+            }
+            EstaOrdenado = true;
+        }
+
+        // Ordenación por Quicksort.
+        public void OrdenarQuickSort()
+        {
+            QuickSort(0, _tope - 1);
+            EstaOrdenado = true;
+        }
+
+        private void QuickSort(int inicio, int fin)
+        {
+            if (inicio >= fin)
+                return;
+
+            int pivote = _arreglo[inicio];
+            int izquierda = inicio + 1;
+            int derecha = fin;
+
+            while (izquierda <= derecha)
+            {
+                while (izquierda <= fin && _arreglo[izquierda] <= pivote) izquierda++;
+                while (derecha > inicio && _arreglo[derecha] > pivote) derecha--;
+
+                if (izquierda < derecha)
+                {
+                    Cambiar(ref _arreglo[izquierda], ref _arreglo[derecha]);
+                }
+            }
+
+            Cambiar(ref _arreglo[inicio], ref _arreglo[derecha]);
+
+            QuickSort(inicio, derecha - 1);
+            QuickSort(derecha + 1, fin);
+        }
+
+        //--------------------------------------------
+        // Métodos de búsqueda solicitados en la asignación
+        //--------------------------------------------
+
+        public int BusquedaSecuencial(int valor)
+        {
+            for (int i = 0; i < _tope; i++)
+            {
+                if (_arreglo[i] == valor)
+                    return i; // Valor encontrado
+            }
+            return -1; // No encontrado
+        }
+
+        public int BusquedaBinaria(int valor)
+        {
+            if (!EstaOrdenado)
+                throw new Exception("El arreglo debe estar ordenado para realizar una búsqueda binaria.");
+
+            int izquierda = 0, derecha = _tope - 1;
+
+            while (izquierda <= derecha)
+            {
+                int medio = (izquierda + derecha) / 2;
+
+                if (_arreglo[medio] == valor)
+                    return medio; // Valor encontrado
+                if (_arreglo[medio] < valor)
+                    izquierda = medio + 1;
+                else
+                    derecha = medio - 1;
+            }
+
+            return -1; // No encontrado
+        }
+
+        //----------------------------------------------
 
         private void Cambiar(ref int a, ref int b)
         {
@@ -138,66 +317,16 @@
             b = a;
             a = aux;
         }
+
         public void Agregar(int numero)
         {
             if (EstaLLeno)
             {
-                throw new Exception("El arreglo esta lleno");   //Lanza una excepcion si el arreglo esta lleno
+                throw new Exception("El arreglo está lleno");
             }
 
             _arreglo[_tope] = numero;
             _tope++;
-        }
-
-        public void Insertar(int numero, int posicion)
-        {
-            if (EstaLLeno)
-            {
-                throw new Exception("El arreglo esta lleno");
-            }
-
-            if (posicion < 0)
-            {
-                posicion = 0;
-            }
-            if (posicion > _tope)
-            {
-                posicion = _tope;
-
-            }
-
-            for (int i = _tope; i > posicion; i--)
-
-            {
-                _arreglo[i] = _arreglo[i - 1];
-
-
-            }
-
-            _arreglo[posicion] = numero;
-            _tope++;
-        }
-
-        public void Eliminar(int posicion)
-        {
-            if (EstaVacio)
-            {
-                throw new Exception("El arreglo esta vacio");
-            }
-            if (posicion < 0)
-            {
-                posicion = 0;
-            }
-
-            if (posicion > _tope)
-            {
-                posicion = _tope;
-            }
-            for (int i = posicion; i < _tope; i++)
-            {
-                _arreglo[i] = _arreglo[i + 1];
-            }
-            _tope--;
         }
 
         public override string ToString()
@@ -206,23 +335,22 @@
 
             if (EstaVacio)
             {
-                return "El arreglo esta vacio";
+                return "El arreglo está vacío";
             }
-            int contador = 0;
+
             for (int i = 0; i < _tope; i++)
             {
                 salida += $"{_arreglo[i]}\t";
-                contador++;
-
-                if (contador > 9)
-                {
-                    salida += "\n";
-                    contador = 0;
-                }
-
             }
+
             return salida;
         }
-
     }
 }
+
+
+
+
+
+
+
--- Arreglos.UI.Consola/Program.cs
+++ Arreglos.UI.Consola/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Arreglos.Logica;
 using System.Diagnostics;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        //PINTAR MENU//
+        Console.WriteLine("Tercer semestre - Estructura de datos");
+        Console.WriteLine("Nombre del Alumno: Jose Sebastian Rodriguez Salgado");
+
         try
         {
             int opcion;
@@ -20,7 +23,7 @@ internal class Program
                         var nString = Console.ReadLine();
                         int n = 10;
                         int.TryParse(nString, out n);
-                        miArreglo = new(n);
+                        miArreglo = new MiArreglo(n);
                         miArreglo.Llenar();
                         break;
 
@@ -33,46 +36,46 @@ internal class Program
 
                     case 3:
                         Console.WriteLine("Mostrar Pares del arreglo:  \n");
+                        Console.WriteLine($"Arreglo Original: {miArreglo}");
                         miArreglo.ObtenerPares();
                         var verPares = miArreglo.ObtenerPares();
                         verPares.Ordenar(true);
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine(verPares + "\n");
+                        Console.WriteLine("Arreglo Resultante: " + verPares);
                         Console.WriteLine("----------------------------");
                         break;
 
                     case 4:
                         Console.WriteLine("Mostrar Impares del arreglo; \n");
+                        Console.WriteLine($"Arreglo Original: {miArreglo}");
                         miArreglo.ObtenerPrimos();
                         var verPrimos = miArreglo.ObtenerPrimos();
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine(verPrimos + "\n");
+                        Console.WriteLine("Arreglo Resultante: " + verPrimos);
                         Console.WriteLine("----------------------------");
                         break;
 
                     case 5:
                         Console.WriteLine("Mostrar números que no se repiten; \n");
-                        var unicos = miArreglo.ObtenerUnicos(); // Método por implementar
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine(unicos);
+                        var unicos = miArreglo.ObtenerUnicos();
+                        Console.WriteLine($"Arreglo Original: {miArreglo}");
+                        Console.WriteLine("Arreglo Resultante: " + unicos);
                         Console.WriteLine("----------------------------");
                         break;
 
                     case 6:
-                        Console.WriteLine("Mostrar números que mas se repiten; \n");
-                        var frecuentes = miArreglo.ObtenerMasFrecuentes(); // Método por implementar
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine(frecuentes);
+                        Console.WriteLine("Mostrar números que más se repiten; \n");
+                        var frecuentes = miArreglo.ObtenerMasFrecuentes();
+                        Console.WriteLine($"Arreglo Original: {miArreglo}");
+                        Console.WriteLine("Arreglo Resultante: " + frecuentes);
                         Console.WriteLine("----------------------------");
                         break;
 
                     case 7:
                         Console.WriteLine("Mostrar Ordenación por Burbuja.; \n");
                         var swBurbuja = Stopwatch.StartNew();
-                        miArreglo.Ordenar(); // Implementado como método por defecto en `MiArreglo`
+                        Console.WriteLine($"Arreglo Original: {miArreglo}");
+                        miArreglo.Ordenar();
                         swBurbuja.Stop();
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine("Arreglo ordenado:\n" + miArreglo);
+                        Console.WriteLine("Arreglo Resultante:\n" + miArreglo);
                         Console.WriteLine($"Tiempo: {FormatoTiempo(swBurbuja.Elapsed)}");
                         Console.WriteLine("----------------------------");
                         break;
@@ -80,31 +83,31 @@ internal class Program
                     case 8:
                         Console.WriteLine("Mostrar Ordenación por Inserción.; \n");
                         var swInsercion = Stopwatch.StartNew();
-                        miArreglo.OrdenarInsercion(); // Método por implementar en `MiArreglo`
+                        Console.WriteLine($"Arreglo Original: {miArreglo}");
+                        miArreglo.OrdenarInsercion();
                         swInsercion.Stop();
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine("Arreglo ordenado:\n" + miArreglo);
+                        Console.WriteLine("Arreglo Resultante:\n" + miArreglo);
                         Console.WriteLine($"Tiempo: {FormatoTiempo(swInsercion.Elapsed)}");
                         Console.WriteLine("----------------------------");
                         break;
 
                     case 9:
-                        Console.WriteLine("Mostrar  Ordenación por Selección.; \n");
+                        Console.WriteLine("Mostrar Ordenación por Selección.; \n");
                         var swSeleccion = Stopwatch.StartNew();
-                        miArreglo.OrdenarSeleccion(); // Método por implementar en `MiArreglo`
+                        Console.WriteLine($"Arreglo Original: {miArreglo}");
+                        miArreglo.OrdenarSeleccion();
                         swSeleccion.Stop();
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine("Arreglo ordenado:\n" + miArreglo);
+                        Console.WriteLine("Arreglo Resultante:\n" + miArreglo);
                         Console.WriteLine($"Tiempo: {FormatoTiempo(swSeleccion.Elapsed)}");
                         Console.WriteLine("----------------------------");
                         break;
                     case 10:
                         Console.WriteLine("Ordenar por Shell:");
                         var swShell = Stopwatch.StartNew();
-                        miArreglo.OrdenarShell(); // Método por implementar en `MiArreglo`
+                        Console.WriteLine($"Arreglo Original: {miArreglo}");
+                        miArreglo.OrdenarShell();
                         swShell.Stop();
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine("Arreglo ordenado:\n" + miArreglo);
+                        Console.WriteLine("Arreglo Resultante:\n" + miArreglo);
                         Console.WriteLine($"Tiempo: {FormatoTiempo(swShell.Elapsed)}");
                         Console.WriteLine("----------------------------");
                         break;
@@ -112,10 +115,10 @@ internal class Program
                     case 11:
                         Console.WriteLine("Mostrar Ordenación por Quicksort; \n");
                         var swQuickSort = Stopwatch.StartNew();
-                        miArreglo.OrdenarQuickSort(); // Método por implementar en `MiArreglo`
+                        Console.WriteLine($"Arreglo Original: {miArreglo}");
+                        miArreglo.OrdenarQuickSort();
                         swQuickSort.Stop();
-                        Console.WriteLine("----------------------------");
-                        Console.WriteLine("Arreglo ordenado:\n" + miArreglo);
+                        Console.WriteLine("Arreglo Resultante:\n" + miArreglo);
                         Console.WriteLine($"Tiempo: {FormatoTiempo(swQuickSort.Elapsed)}");
                         Console.WriteLine("----------------------------");
                         break;
@@ -123,7 +126,7 @@ internal class Program
                     case 12:
                         Console.WriteLine("Mostrar Búsqueda Secuencial.; \n");
                         var valorSec = int.Parse(Console.ReadLine());
-                        var indexSec = miArreglo.BusquedaSecuencial(valorSec); // Método por implementar
+                        var indexSec = miArreglo.BusquedaSecuencial(valorSec);
                         Console.WriteLine("----------------------------");
                         Console.WriteLine(indexSec != -1
                             ? $"Valor encontrado en el índice: {indexSec}"
@@ -134,12 +137,16 @@ internal class Program
                     case 13:
                         Console.WriteLine("Mostrar Búsqueda Binaria.; \n");
                         var valorBin = int.Parse(Console.ReadLine());
-                        var indexBin = miArreglo.BusquedaBinaria(valorBin); // Método por implementar
+                        var indexBin = miArreglo.BusquedaBinaria(valorBin);
                         Console.WriteLine("----------------------------");
                         Console.WriteLine(indexBin != -1
                             ? $"Valor encontrado en el índice: {indexBin}"
                             : "Valor no encontrado.");
                         Console.WriteLine("----------------------------");
+                        break;
+
+                    case 0:
+                        Console.WriteLine("¡Programa terminado!");
                         break;
                 }
 
@@ -147,60 +154,46 @@ internal class Program
         }
         catch (Exception ex)
         {
-
             Console.WriteLine(ex.Message);
         }
+    }
 
-        int menu()
+    static int menu()
+    {
+        // Menú de opciones
+        Console.WriteLine("1. Definir el tamaño del arreglo.");
+        Console.WriteLine("2. Mostrar arreglo.");
+        Console.WriteLine("3. Mostrar números pares.");
+        Console.WriteLine("4. Mostrar números primos.");
+        Console.WriteLine("5. Mostrar números que no se repiten.");
+        Console.WriteLine("6. Mostrar números que más se repiten.");
+        Console.WriteLine("7. Ordenación por Burbuja.");
+        Console.WriteLine("8. Ordenación por Inserción.");
+        Console.WriteLine("9. Ordenación por Selección.");
+        Console.WriteLine("10. Ordenación por Shell.");
+        Console.WriteLine("11. Ordenación por Quicksort.");
+        Console.WriteLine("12. Búsqueda Secuencial.");
+        Console.WriteLine("13. Búsqueda Binaria.");
+        Console.WriteLine("0. Salir");
+
+        int op = 0;
+        do
         {
-            //Pintar menu//
-            Console.WriteLine("1. Definir el arreglo.");
-            Console.WriteLine("2. Mostrar arreglo.");
-            Console.WriteLine("3. Mostrar numeros pares.");
-            Console.WriteLine("4. Mostrar numeros primos.");
-            Console.WriteLine("5. Mostrar numeros que no se repitren.");
-            Console.WriteLine("6. Mostrar numeros que mas se repiten.");
-            Console.WriteLine("7. Ordenación por Burbuja.");
-            Console.WriteLine("8. Ordenación por Inserción.");
-            Console.WriteLine("9. Ordenación por Selección.8");
-            Console.WriteLine("10. Ordenación por Shell.");
-            Console.WriteLine("11. Ordenación por Quicksort.");
-            Console.WriteLine("12. Búsqueda Secuencial.");
-            Console.WriteLine("13. Búsqueda Binaria.");
-            Console.WriteLine("0. Salir");
-
-            bool EsValido = false;
-
-            int op = 0;
-
-            //Validar si la opcion del usuario es correcta//
-            do
+            Console.WriteLine("\n Seleccione una opción del menú: ");
+            var opCadena = Console.ReadLine();
+            if (!int.TryParse(opCadena, out op) || op < 0 || op > 13)
             {
-                Console.WriteLine("\n Seleccione una opcion del menu: ");
-                var opCadena = Console.ReadLine();
+                Console.WriteLine("\n Selecciona una opción válida.");
+            }
+        } while (op < 0 || op > 13);
 
-                if (!int.TryParse(opCadena, out op) || op < 0 || op > 13)
-                {
-                    Console.WriteLine("\n Selecciona una opción correcta, use solo numeros");
-                    EsValido = false;
-                }
-                else
-                {
-                    EsValido = true;
-                }
+        return op;
+    }
 
-            } while (EsValido == false);
-
-            return op;
-
-
-        }
-
-        private static string FormatoTiempo(TimeSpan tiempo)
-        {
-            return $"{tiempo.Hours}h:{tiempo.Minutes}m:{tiempo.Seconds}s:{tiempo.Milliseconds}ms";
-        }
-
-        Console.ReadKey();
+    private static string FormatoTiempo(TimeSpan tiempo)
+    {
+        return $"{tiempo.Hours}h:{tiempo.Minutes}m:{tiempo.Seconds}s:{tiempo.Milliseconds}ms";
     }
 }
+
+
